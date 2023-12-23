d09e6270f81cb773c692bd4f3140c3e3334b1dec
Dennis Hackethal <dh@dennishackethal.com>
2023-12-23 15:57:09 -0700

    Housekeeping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 1.0f;
+    public float speed;
     public GameObject powerupIndicator;
 
     private bool hasPowerup;
